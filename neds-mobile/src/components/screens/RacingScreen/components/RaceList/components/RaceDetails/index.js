13962b8820136cc6a777d9fd7181aca1b493503b
Chris Rabe <chris.rabe14@gmail.com>
2021-01-21 15:44:57 +1000

    update display time every second

--- neds-mobile/src/components/screens/RacingScreen/components/RaceList/components/RaceDetails/index.js
+++ neds-mobile/src/components/screens/RacingScreen/components/RaceList/components/RaceDetails/index.js
@@ -1,34 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
 import { ListItem, Text, Left, Right } from "native-base";
 import CustomIcon from "@neds/components/common/CustomIcon";
 import styles from "./styles";
 import dayjs from "dayjs";
+import useInterval from "@neds/hook/useInterval";
 
 const RaceDetails = ({ meetingName, raceNumber, category, startTime }) => {
   const [displayTime, setDisplayTime] = useState("?");
 
+  const updateDisplayTime = useCallback(() => {
+    // get minute difference between now and start time
+    const minuteDiff = startTime.diff(dayjs(), "minutes");
+    if (minuteDiff > 60) {
+      // show hours, minutes
+      const hours = Math.floor(minuteDiff / 60);
+      const minutes = minuteDiff - hours * 60;
+      const timeStr = `${hours}h ${minutes}m`;
+      setDisplayTime(timeStr);
+    } else {
+      // show minutes, seconds
+      const secsDiff = startTime.diff(dayjs(), "seconds");
+      const absDiff = Math.abs(secsDiff);
+      const minutes = Math.floor(absDiff / 60);
+      const seconds = absDiff - minutes * 60;
+      const timeStr = `${minutes}m ${seconds}s`;
+      setDisplayTime(secsDiff > 0 ? timeStr : `-${timeStr}`);
+    }
+  }, [setDisplayTime, startTime]);
+
   useEffect(() => {
     if (startTime) {
-      // get minute difference between now and start time
-      const minuteDiff = startTime.diff(dayjs(), "minutes");
-      if (minuteDiff > 60) {
-        // show hours, minutes
-        const hours = Math.floor(minuteDiff / 60);
-        const minutes = minuteDiff - hours * 60;
-        const timeStr = `${hours}h ${minutes}m`;
-        setDisplayTime(timeStr);
-      } else {
-        // show minutes, seconds
-        const secsDiff = startTime.diff(dayjs(), "seconds");
-        const absDiff = Math.abs(secsDiff);
-        const minutes = Math.floor(absDiff / 60);
-        const seconds = absDiff - minutes * 60;
-        const timeStr = `${minutes}m ${seconds}s`;
-        setDisplayTime(secsDiff > 0 ? timeStr : `-${timeStr}`);
-      }
+      updateDisplayTime();
     }
-  }, [setDisplayTime]);
+  }, [updateDisplayTime]);
+
+  // every second, display time is updated
+  useInterval(() => {
+    if (startTime) {
+      updateDisplayTime();
+    }
+  }, 1000);
 
   // calculate start time
   return (
